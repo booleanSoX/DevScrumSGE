cbbafce4ab26833ef9145f6a1cb5a38ca18c834f
Sebas <26633037a@iespoblenou.org>
2025-12-18 14:28:54 +0100

    Creacion de Variables iniciales

--- ProyectoSCRUM/src/Scrum.java
+++ ProyectoSCRUM/src/Scrum.java
@@ -1,9 +1,128 @@
 import java.util.*;
 public class Scrum {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hola Mundo");
+        Scanner sc = new Scanner(System.in);
+        
+        
+        ///////////////VARIABLES////////////////
+        //Declaramos el mapa 
+        int[][] mapa = new int[10][10];
+
+        //Declaramos el array y lo rellenamos con la coordenadas de la incidencia        
+        int lugarIncidenciaCoords[] = new int[2];
+        boolean disponibilidadVehiculo;
+        int tiempoRespuesta;
+        char tipoVehiculo;
+        HashMap<Character, Integer> vehiculoID = new HashMap<Character, Integer>();
+        HashMap<Integer, Integer[][]> vehiculoCoords = new HashMap<Integer, int[][]>();
+
+
+
+
+
+        /////////////////INPUTS DE USUARIO///////////
+        //Le pedimos al usuario que nos introduzca las coordenadas del incidente        
+        System.out.println("Dame la ubicación de la incidencia:");
+        int coordenadaIncidenciaX = sc.nextInt();
+        int coordenadaIncidenciay = sc.nextInt(); 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        ///////////INTRODUCCION DE DATOS EN VARIABLES///////////
+        lugarIncidenciaCoords[0] = coordenadaIncidenciaX;
+        lugarIncidenciaCoords[1] = coordenadaIncidenciay;
+
+
+
+
+
+
+
+
+
+
+
+
+        /////////////OUTPUTS///////////
+        
+        //Impresion del mapa con las coordenadas
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa.length; j++) {
+                System.out.print("[" + i + "]" + "[" + j + "] ");
+            }
+            System.out.println(" ");
         }
+
+
+
+
+
+
+
+
+                sc.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
