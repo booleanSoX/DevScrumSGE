7e233bebd9304c0a7ef54318f0595c9bbed2a8a8
Sebas <26633037a@iespoblenou.org>
2025-12-18 12:18:16 +0100

    Agregado del proyecto en Java.

---
+++ ProyectoSCRUM/src/Scrum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Scrum {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hola Mundo");
+        }
+
+
+    }
+}
